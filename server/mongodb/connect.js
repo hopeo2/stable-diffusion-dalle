13f2004f72e53498852071e84adbce2a5dc0e2be
omidobeidzadeh <omidobeidzadeh@gmail.com>
2023-09-19 05:06:58 +0330

    change

---
+++ server/mongodb/connect.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const connectDB = (url) => {
+    mongoose.set('strictQuery', true)
+
+    mongoose.connect(url).then(
+        () => {
+            return console.log('MongoDB connected');
+        }
+    ).catch(
+        (err) => {
+            return console.log(err);
+        }
+    )
+}
+
+export default connectDB;
